ce40da992ac7243b14ff6787230f192df51db72d
Cristian Mateos <mateos.c19@gmail.com>
2020-09-30 01:00:09 +0200

    test prompt

--- skyline.zsh
+++ skyline.zsh
@@ -2,5 +2,5 @@ setopt PROMPT_SUBST
 autoload colors
 colors
 
-PROMPT=$'%{$fg[red]%}\uf438%{$bg[red]}\uf489 %k\uf44a%{$reset_color%}'
+PROMPT=$'%{$fg[red]%}\uf438%f%{$bg[red]%}\uf489 %k%{$fg[red]%}\uf44a%{$reset_color%}'
 RPROMPT=" <>"
