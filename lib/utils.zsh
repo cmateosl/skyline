639406183a601fed285fd73a8af462f370901884
Cristian Mateos <mateos.c19@gmail.com>
2020-09-30 20:41:24 +0200

    test prompt

---
+++ lib/utils.zsh
@@ -0,0 +1,17 @@
+skyline::color() {
+  local content="$1" fg_color="$2" bg_color="$3"
+  local reset
+  
+  if [[ -n $fg_color ]]; then
+    echo -n "%F{$fg_color}"
+    reset="${reset}%f"
+  fi
+  
+  if [[ -n $bg_color ]]; then
+    echo -n "%K{$bg_color}"
+    reset="${reset}%k"
+  fi
+
+  echo -n $content
+  echo -n $reset
+}
