aa936e3eedde91cd8d204534ba0b5a3c92468806
Cristian Mateos <mateos.c19@gmail.com>
2020-10-02 18:42:14 +0200

    test prompt

--- section/git.zsh
+++ section/git.zsh
@@ -1,37 +1,16 @@
-SKYLINE_GIT_STATUS_UNTRACKED="${SKYLINE_GIT_STATUS_UNTRACKED="?"}"
-SPACESHIP_GIT_STATUS_ADDED="${SPACESHIP_GIT_STATUS_ADDED="+"}"
-SKYLINE_GIT_STATUS_MODIFIED="${SKYLINE_GIT_STATUS_MODIFIED="!"}"
+SKYLINE_GIT_STATUS_UNTRACKED="${SKYLINE_GIT_STATUS_UNTRACKED="?"}" # nf-fa-question_circle
+SKYLINE_GIT_STATUS_ADDED="${SKYLINE_GIT_STATUS_ADDED="+"}" # nf-fa-plus_circle
+SKYLINE_GIT_STATUS_MODIFIED="${SKYLINE_GIT_STATUS_MODIFIED="!"}" # nf-fa-exclamation_circle
+SKYLINE_GIT_STATUS_RENAMED="${SKYLINE_GIT_STATUS_RENAMED="»"}" # nf-fa-arrow_circle_righ
+SKYLINE_GIT_STATUS_DELETED="${SKYLINE_GIT_STATUS_DELETED="✘"}" #nf-fa-times_circle
+SKYLINE_GIT_STATUS_STASHED="${SKYLINE_GIT_STATUS_STASHED="$"}" 
+SKYLINE_GIT_STATUS_UNMERGED="${SKYLINE_GIT_STATUS_UNMERGED="="}"
+SKYLINE_GIT_STATUS_AHEAD="${SKYLINE_GIT_STATUS_AHEAD="⇡"}"
+SKYLINE_GIT_STATUS_BEHIND="${SKYLINE_GIT_STATUS_BEHIND="⇣"}"
+SKYLINE_GIT_STATUS_DIVERGED="${SKYLINE_GIT_STATUS_DIVERGED="⇕"}"
 
 skyline::git() {
-  local git_current_branch="$vcs_info_msg_0_"
-  [[ -z "$git_current_branch" ]] && return
-
-  skyline::bold "on "
-  skyline::color $git_current_branch green
-
-  local INDEX git_status=""
-  INDEX=$(command git status --porcelain -b 2> /dev/null)
+  source "$SKYLINE_ROOT/section/git-branch.zsh"
   
-  local count
-  # Check for untracked files
-  count="$(echo "$INDEX" | command grep -E '^\?\? ' &> /dev/null | wc -l | awk '{$1=$1};1')"
-  (( $count > 0 )) && git_status="$SKYLINE_GIT_STATUS_UNTRACKED$count$git_status"
-
-  # Check for staged files
-  if $(echo "$INDEX" | command grep -E '^A[ MDAU] ' &> /dev/null); then
-    count="$(echo "$INDEX" | command grep -E '^A[ MDAU] ' &> /dev/null | wc -l | awk '{$1=$1};1')"
-    git_status="$SPACESHIP_GIT_STATUS_ADDED$count $git_status"
-  elif $(echo "$INDEX" | command grep '^M[ MD] ' &> /dev/null); then
-    count="$(echo "$INDEX" | command grep '^M[ MD] ' &> /dev/null | wc -l | awk '{$1=$1};1')"
-    git_status="$SPACESHIP_GIT_STATUS_ADDED$count $git_status"
-  elif $(echo "$INDEX" | command grep '^UA' &> /dev/null); then
-    count="$(echo "$INDEX" | command grep '^UA' &> /dev/null | wc -l | awk '{$1=$1};1')"
-    git_status="$SPACESHIP_GIT_STATUS_ADDED$count $git_status"
-  fi
-
-  # Check for modified files
-  count="$(echo "$INDEX" | command grep '^[ MARC]M ' &> /dev/null | wc -l | awk '{$1=$1};1')"
-  (( $count > 0 )) && git_status="$SKYLINE_GIT_STATUS_MODIFIED$count $git_status"
-
-  echo -n " $git_status"
+  skyline::git_branch
 }
