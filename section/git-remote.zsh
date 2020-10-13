d9ecb65d6790330708dbb1072d5e65c595fb24b4
Cristian Mateos <mateos.c19@gmail.com>
2020-10-13 12:30:58 +0200

    Setup git section

--- section/git-remote.zsh
+++ section/git-remote.zsh
@@ -1,18 +1,18 @@
 SKYLINE_VCS_REMOTE_PREFIX="${SKYLINE_VCS_REMOTE_PREFIX= }"
 SKYLINE_VCS_REMOTE_SUFFIX="${SKYLINE_VCS_REMOTE_SUFFIX= }"
-SKYLINE_GIT_GITHUB_ICON="${SKYLINE_GIT_GITHUB_ICON=\uf113}"
-SKYLINE_GIT_BITBUCKET_ICON="${SKYLINE_GIT_BITBUCKET_ICON=\uf171}"
-SKYLINE_GIT_GITLAB_ICON="${SKYLINE_GIT_GITLAB_ICON=\uf296}"
-SKYLINE_GIT_ICON="${SKYLINE_GIT_ICON=\uf1d3}"
+SKYLINE_VCS_GITHUB_ICON="${SKYLINE_VCS_GITHUB_ICON=\uf113}"
+SKYLINE_VCS_BITBUCKET_ICON="${SKYLINE_VCS_BITBUCKET_ICON=\uf171}"
+SKYLINE_VCS_GITLAB_ICON="${SKYLINE_VCS_GITLAB_ICON=\uf296}"
+SKYLINE_VCS_GIT_ICON="${SKYLINE_VCS_GIT_ICON=\uf7a1}"
 
 skyline::git_remote_icon() {
   local icon
   case "$SKYLINE_VCS_STATUS_REMOTE_URL" in
-    *github*)     icon="${SKYLINE_GIT_GITHUB_ICON} GitHub";;
-    *bitbucket*)  icon="${SKYLINE_GIT_BITBUCKET_ICON} Bitbucket";;
-    *stash*)      icon="${SKYLINE_GIT_GITHUB_ICON} GitHub";;
-    *gitlab*)     icon="${SKYLINE_GIT_GITLAB_ICON} GitLab";;
-    *)            icon="${SKYLINE_GIT_ICON} Git";;
+    *github*)     icon="${SKYLINE_VCS_GITHUB_ICON} GitHub";;
+    *bitbucket*)  icon="${SKYLINE_VCS_BITBUCKET_ICON} Bitbucket";;
+    *stash*)      icon="${SKYLINE_VCS_GITHUB_ICON} GitHub";;
+    *gitlab*)     icon="${SKYLINE_VCS_GITLAB_ICON} GitLab";;
+    *)            icon="${SKYLINE_VCS_GIT_ICON} Git";;
   esac
 
   skyline::section \
