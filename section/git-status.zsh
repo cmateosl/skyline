4cbe3346003adca0625b07fc59e2e1e75262419c
Cristian Mateos López <mateos.c19@gmail.com>
2020-10-15 11:14:37 +0200

    Add staged icon

--- section/git-status.zsh
+++ section/git-status.zsh
@@ -1,24 +1,25 @@
 SKYLINE_VCS_STATUS_UNTRACKED_ICON="${SKYLINE_VCS_STATUS_UNTRACKED_ICON=\uf440}"
 SKYLINE_VCS_STATUS_MODIFIED_ICON="${SKYLINE_VCS_STATUS_MODIFIED_ICON=\uf040}"
+SKYLINE_VCS_STATUS_STAGED_ICON="${SKYLINE_VCS_STATUS_STAGED_ICON=\uf417}"
 
 typeset SKYLINE_VCS_STATUS_LOCAL_BRANCH
-typeset SKYLINE_VCS_STATUS_REMOTE_BRANCH
 typeset SKYLINE_VCS_STATUS_REMOTE_NAME
 typeset SKYLINE_VCS_STATUS_REMOTE_URL
+typeset SKYLINE_VCS_STATUS
 typeset SKYLINE_VCS_STATUS_UNTRACKED_FILES
 typeset SKYLINE_VCS_STATUS_MODIFIED_FILES
-typeset SKYLINE_VCS_STATUS
+typeset SKYLINE_VCS_STATUS_STAGED_FILES
 
 skyline::git_status() {
-  SKYLINE_VCS_STATUS_LOCAL_BRANCH=`git branch --show-current`
+  SKYLINE_VCS_STATUS_LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
-  SKYLINE_VCS_STATUS_REMOTE_BRANCH=`git config branch.$SKYLINE_VCS_STATUS_LOCAL_BRANCH.merge`
-  SKYLINE_VCS_STATUS_REMOTE_NAME=`git config branch.$SKYLINE_VCS_STATUS_LOCAL_BRANCH.remote`
-  SKYLINE_VCS_STATUS_REMOTE_URL=`git config remote.$SKYLINE_VCS_STATUS_REMOTE_NAME.url`
+  SKYLINE_VCS_STATUS_REMOTE_NAME=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} | cut -d"/" -f1)
+  SKYLINE_VCS_STATUS_REMOTE_URL=$(git config remote.$SKYLINE_VCS_STATUS_REMOTE_NAME.url)
 
-  SKYLINE_VCS_STATUS=`git status -s 2> /dev/null`
-  SKYLINE_VCS_STATUS_UNTRACKED_FILES=`git ls-files --others --exclude-standard 2> /dev/null`
-  SKYLINE_VCS_STATUS_MODIFIED_FILES=`git ls-files -m 2> /dev/null`
+  SKYLINE_VCS_STATUS=$(git status -s 2> /dev/null)
+  SKYLINE_VCS_STATUS_UNTRACKED_FILES=$(git ls-files --others --exclude-standard 2> /dev/null)
+  SKYLINE_VCS_STATUS_MODIFIED_FILES=$(git ls-files -m 2> /dev/null)
+  SKYLINE_VCS_STATUS_STAGED_FILES=$(git diff --name-only --cached 2> /dev/null)
 }
 
 skyline::git_status_icons() {
@@ -30,6 +31,10 @@ skyline::git_status_icons() {
 
   if [[ ! -z $SKYLINE_VCS_STATUS_MODIFIED_FILES ]]; then
     content="$SKYLINE_VCS_STATUS_MODIFIED_ICON $content"
+  fi
+
+  if [[ ! -z $SKYLINE_VCS_STATUS_STAGED_FILES ]]; then
+    content="$SKYLINE_VCS_STATUS_STAGED_ICON $content"
   fi
 
   skyline::section 228 $content " " ""
