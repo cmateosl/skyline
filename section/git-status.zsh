975a5302e387a4272227f35cbe103f73588062b9
Cristian Mateos López <mateos.c19@gmail.com>
2020-10-15 11:28:41 +0200

    Add stash icon

--- section/git-status.zsh
+++ section/git-status.zsh
@@ -1,6 +1,7 @@
 SKYLINE_VCS_STATUS_UNTRACKED_ICON="${SKYLINE_VCS_STATUS_UNTRACKED_ICON=\uf440}"
 SKYLINE_VCS_STATUS_MODIFIED_ICON="${SKYLINE_VCS_STATUS_MODIFIED_ICON=\uf040}"
 SKYLINE_VCS_STATUS_STAGED_ICON="${SKYLINE_VCS_STATUS_STAGED_ICON=\uf417}"
+SKYLINE_VCS_STATUS_STASH_ICON="${SKYLINE_VCS_STATUS_STASH_ICON=\u}"
 
 typeset SKYLINE_VCS_STATUS_LOCAL_BRANCH
 typeset SKYLINE_VCS_STATUS_REMOTE_NAME
@@ -9,6 +10,7 @@ typeset SKYLINE_VCS_STATUS
 typeset SKYLINE_VCS_STATUS_UNTRACKED_FILES
 typeset SKYLINE_VCS_STATUS_MODIFIED_FILES
 typeset SKYLINE_VCS_STATUS_STAGED_FILES
+typeset SKYLINE_VCS_STATUS_STASH_FILES
 
 skyline::git_status() {
   SKYLINE_VCS_STATUS_LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
@@ -20,6 +22,7 @@ skyline::git_status() {
   SKYLINE_VCS_STATUS_UNTRACKED_FILES=$(git ls-files --others --exclude-standard 2> /dev/null)
   SKYLINE_VCS_STATUS_MODIFIED_FILES=$(git ls-files -m 2> /dev/null)
   SKYLINE_VCS_STATUS_STAGED_FILES=$(git diff --name-only --cached 2> /dev/null)
+  SKYLINE_VCS_STATUS_STASH_FILES=$(git stash list 2> /dev/null)
 }
 
 skyline::git_status_icons() {
@@ -35,6 +38,10 @@ skyline::git_status_icons() {
 
   if [[ ! -z $SKYLINE_VCS_STATUS_STAGED_FILES ]]; then
     content="$SKYLINE_VCS_STATUS_STAGED_ICON $content"
+  fi
+
+  if [[ ! -z $SKYLINE_VCS_STATUS_STASH_FILES ]]; then
+    content="$SKYLINE_VCS_STATUS_STASH_ICON $content"
   fi
 
   skyline::section 228 $content " " ""
